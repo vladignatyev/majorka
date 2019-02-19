a3b26531a9190297051b2dbfa8de7c84085b3df3
Vladimir Ignatev <ya.na.pochte@gmail.com>
2019-02-19 12:41:17 +0400

    added logging, removed unused dependencies

--- smoke.py
+++ smoke.py
@@ -1,7 +1,7 @@
-
+import logging
+from datetime import timedelta, datetime
 import json
 import requests
-
 
 
 class PropellerAds(object):
@@ -18,14 +18,28 @@ class PropellerAds(object):
         STOPPED = 8
         COMPLETED = 9
 
-        ANY = [DRAFT, MODERATION, REJECTED, READY, TEST_RUN, WORKING, PAUSED, STOPPED, COMPLETED]
+        ALL = [DRAFT, MODERATION, REJECTED, READY, TEST_RUN, WORKING, PAUSED, STOPPED, COMPLETED]
 
-    def __init__(self, username, password):
+        @classmethod
+        def is_valid_status(cls, *statuses):
+            return all([x in cls.ALL for x in statuses])
+
+    def __init__(self, username, password, logger=logging.getLogger('propellerads')):
         self.username = username
         self.password = password
         self._token = None
+        self._token_will_expire = None
+        self.log = logger
+
+        self.log.info("Api Initialized")
 
     def authorize(self, urlpath='/adv/login'):
+        self.log.info("Authorization with credentials: %s / %s", self.username, '*' * len(self.password))
+
+        if self.is_authorized():
+            self.log.info("The user %s is already authorized! Token will expire on %s", self.username, self.when_authorization_will_expire())
+            return
+
         target_url = "%s%s" % (self.REST_URL, urlpath)
         payload = json.dumps({'username': self.username, 'password': self.password})
         headers = {
@@ -35,9 +49,18 @@ class PropellerAds(object):
 
         json_response = self._error_or_result(requests.request("POST", target_url, data=payload, headers=headers))
         self._token = json_response['api_token']
+        self._token_will_expire = datetime.now() + timedelta(seconds=int(json_response['expires_in']))
+        self.log.info("New authorization token acquired. Will expire on %s", self.when_authorization_will_expire())
 
     def is_authorized(self):
-        return self._token is not None
+        if self._token is None or self._token_will_expire is None:
+            return False
+        if datetime.now() > self._token_will_expire:
+            return False
+        return True
+
+    def when_authorization_will_expire(self):
+        return self._token_will_expire
 
     def _auth_headers(self):
         return { 'Authorization': 'Bearer %s' % self._token }
@@ -46,27 +69,39 @@ class PropellerAds(object):
 
     def _error_or_result(self, response):
         response_obj = json.loads(response.text)
+        self.log.debug("Received: %s", response.text)
+        self.log.info("Status %s", response.status_code)
 
         if "error" in response_obj.keys() or "errors" in response_obj.keys():
+            self.log.critical("Request to API returned an error: %s %s", response_obj.get('message', 'Error'), response_obj['errors'], extra={'response': response_obj})
             raise Exception("'%s': %s" % (response_obj.get('message', 'Error'), response_obj['errors']))
 
         return response_obj
 
+    # todo: add support of multiple pages of response
+    # !!! DRAFT !!!
     def campaigns_all(self, urlpath='/adv/campaigns'):
+        self.log.info("Requesting info about campaigns")
         target_url = "%s%s" % (self.REST_URL, urlpath)
         return self._error_or_result(requests.request("GET", target_url, headers=self._auth_headers()))
-
+    #
+    # # !!! DRAFT !!!
     def campaigns_by_statuses(self, *statuses):
-        if not all([x in self.Status.ANY for x in statuses]):
+        self.log.info("Requesting info about campaigns with statuses: %s", statuses)
+        if not self.Status.is_valid_status(*statuses):
+            self.log.critical("Invalid statuses provided %s", statuses)
             raise Exception('Invalid status provided')
         status_str = '&'.join(["status[]=%s" % str(x) for x in statuses])
 
         urlpath = '/adv/campaigns?%s' % status_str
+        self.log.debug("The destination URL will be: %s", urlpath)
         return self.campaigns_all(urlpath=urlpath)
 
     def campaign_stop_by_id(self, *campaign_ids, **kwargs):
         if len(campaign_ids) == 0:
             return None
+
+        self.log.info("Stopping campaigns by ID: %s", campaign_ids)
 
         urlpath = kwargs.get('urlpath', '/adv/campaigns/stop')
         target_url = "%s%s" % (self.REST_URL, urlpath)
@@ -76,20 +111,51 @@ class PropellerAds(object):
         return self._error_or_result(requests.request("PUT", target_url, headers=headers, data=payload))
 
     def campaign_start_by_id(self, *campaign_ids, **kwargs):
+        self.log.info("Starting campaigns by ID: %s", campaign_ids)
         return self.campaign_stop_by_id(*campaign_ids, urlpath='/adv/campaigns/play')
 
+    def campaign_get_include_zones(self, campaign_id, urlpath='/adv/campaigns/{campaign_id}/targeting/include/zone?campaignId={campaign_id}'):
+        self.log.info("Getting 'include' zone targeting for campaign: %s", campaign_id)
+        target_url = "%s%s" % (self.REST_URL, urlpath.format(**{'campaign_id': campaign_id}))
+        headers = dict(self._auth_headers(), **self._content_type())
+        result = self._error_or_result(requests.request("GET", target_url, headers=headers))
+        return result['zone']
 
+    def campaign_get_exclude_zones(self, campaign_id, urlpath='/adv/campaigns/{campaign_id}/targeting/exclude/zone?campaignId={campaign_id}'):
+        self.log.info("Getting 'exclude' zone targeting for campaign: %s", campaign_id)
+        return self.campaign_get_include_zones(campaign_id, urlpath=urlpath)
+
+    def campaign_info_by_id(self, campaign_id, urlpath='/adv/campaigns/{campaign_id}'):
+        self.log.info("Getting full campaign info for campaign: %s", campaign_id)
+        target_url = "%s%s" % (self.REST_URL, urlpath.format(**{'campaign_id': campaign_id}))
+        headers = dict(self._auth_headers(), **self._content_type())
+        return self._error_or_result(requests.request("GET", target_url, headers=headers))
+
+
+
+logging.basicConfig(format='[%(name)s] %(asctime)-15s %(message)s')
+logger = logging.getLogger('propellerads')
+logger.setLevel(logging.INFO)
 
 from credentials import credentials
-
-p = PropellerAds(**credentials)
+p = PropellerAds(credentials['username'], credentials['password'], logger=logger)
 p.authorize()
 assert p.is_authorized()
+
+p.campaigns_by_statuses(PropellerAds.Status.WORKING)
+p.campaigns_by_statuses(PropellerAds.Status.STOPPED)
 
 # print json.dumps(p.campaigns_by_statuses(PropellerAds.Status.WORKING), indent=4, sort_keys=True)
 
 # print json.dumps(p.campaigns_by_statuses(PropellerAds.Status.STOPPED), indent=4, sort_keys=True)
 # print p.campaign_start_by_id(1790281)
 
-# print p.campaign_stop_by_id(1790281)
+# p.campaign_stop_by_id(1791408)
 # print p.campaign_start_by_id(1790281)
+
+# print p.campaign_get_exclude_zones(1734282)
+# assert len(p.campaign_get_exclude_zones(1734282)) == 25
+
+print p.campaign_info_by_id(1734282)
+
+# print p.campaigns_by_statuses(PropellerAds.Status.STOPPED)
