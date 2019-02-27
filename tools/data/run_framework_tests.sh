f5ae86d289f23afb34997454bcf1a37f19510f45
Vladimir Ignatev <ya.na.pochte@gmail.com>
2019-02-27 14:51:12 +0400

    more accurate coverage report for #3

--- tools/data/run_framework_tests.sh
+++ tools/data/run_framework_tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if $(test coverage); then
-  coverage erase && coverage run --branch --source . -m framework.tests
+  coverage erase && coverage run --branch --source framework/ -m framework.tests
   coverage report -m --skip-covered
 else
   python -m framework.tests
