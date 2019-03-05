c39295e642edd55a70a12ff1086f6c71531ebfc1
Vladimir Ignatev <ya.na.pochte@gmail.com>
2019-03-05 13:18:14 +0400

    fixed remove_all_dirs fail (urls_github.txt -> urls.txt)

--- prelanders/remove_all_dirs.sh
+++ prelanders/remove_all_dirs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-while IFS=: read -r url	
+while IFS=: read -r url
 do
     echo URL $url
-    dirname=`echo $url | cut -d "?" -f1 | awk -F "pro/" '{print $2}'` 
+    dirname=`echo $url | cut -d "?" -f1 | awk -F "pro/" '{print $2}'`
     rm -rf $dirname
     echo DIRNANE $dirname
-done <"urls_github.txt"
+done <"urls.txt"
