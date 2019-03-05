f1df8cb1c3cb31f5f178a47c92539d4b4df45ae8
Vladimir Ignatev <ya.na.pochte@gmail.com>
2019-03-05 13:33:14 +0400

    less verbose output

--- prelanders/remove_httrack.sh
+++ prelanders/remove_httrack.sh
@@ -3,7 +3,8 @@ while IFS=: read -r url
 do
     echo URL $url
     dirname=`echo $url | cut -d "?" -f1 | awk -F "pro/" '{print $2}'`
-    mkdir -p $dirname
     echo DIRNAME $dirname
-    httrack --sockets=32 --structure=100 --get-files $url -O $dirname > $dirname/httrack_command_log.txt
+    rm -rf $dirname/httrack_command_log.txt
+    rm -rf $dirname/*.readme
+    mv $dirname/*.html $dirname/index.html
 done <"urls.txt"
