3ed917711f705baa997ef53c2ccb47b12497ed8e
Vladimir Ignatev <ya.na.pochte@gmail.com>
2019-03-06 12:16:46 +0400

    cleaned up landings

--- prelanders/download_landings.sh
+++ prelanders/download_landings.sh
@@ -5,5 +5,5 @@ do
     dirname=`echo $url | cut -d "?" -f1 | awk -F "pro/" '{print $2}'`
     mkdir -p $dirname
     echo DIRNAME $dirname
-    httrack --sockets=32 --structure=100 --get-files $url -O $dirname > $dirname/httrack_command_log.txt
+    httrack --sockets=32 --structure=100 $url -O $dirname > $dirname/httrack_command_log.txt
 done <"urls.txt"
--- prelanders/remove_httrack.sh
+++ prelanders/remove_httrack.sh
@@ -5,6 +5,13 @@ do
     dirname=`echo $url | cut -d "?" -f1 | awk -F "pro/" '{print $2}'`
     echo DIRNAME $dirname
     rm -rf $dirname/httrack_command_log.txt
-    rm -rf $dirname/*.readme
-    mv $dirname/*.html $dirname/index.html
+    rm -rf $dirname/cookies.txt
+    rm -rf $dirname/backblue.gif
+    rm -rf $dirname/fade.gif
+    rm -rf $dirname/GET.html
+    rm -rf $dirname/test.html
+    rm -rf $dirname/hts*
+    rm -rf $dirname/OPR
+
+    mv $dirname/$dirname*.html $dirname/index.html
 done <"urls.txt"
