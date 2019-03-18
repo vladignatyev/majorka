f96ce08bc208631688337c00837aeb4199a82229
Vitalii Ruzhnikov <varuzhnikov@gmail.com>
2019-03-18 22:17:35 +0400

    Added cli

--- tools/blacklist.py
+++ tools/blacklist.py
@@ -1,26 +1,26 @@
 from blacklist import *
+import click
 
-def execute():
+@click.command()
+@click.option('--ch-url', envvar='CH_URL', required=True, help='Clickhouse URL, i.e. http://192.168.9.39:8123/. You can use CH_URL environmental variable to set this parameter.')
+@click.option('--info', help='Print blacklist for given campaign')
+@click.option('--campaign', type=click.IntRange(min=0, max=100000), required=True, help='Id for campaign to print blacklist for')
+@click.option('--roi', required=True, type=click.IntRange(min=-100, max=100000), help='minimum ROI for particular slice ( zoneid -> connection_type -> offer)')
+@click.option('--clicks', required=True, type=click.IntRange(min=0, max=10000000000L), help='minimum clicks for every slice required to decide that zone will be included in blacklist')
+def execute(ch_url, info, campaign, roi, clicks):
+    """Prints blacklist for given campaign"""
     from data.framework.reporting import Database
 
-    d = Database(url='http://157.230.19.15/', db='majorka')
+    d = Database(url=ch_url, db='majorka')
 
-    slice_blacklist = SliceBlacklist(campaign_id=0, min_clicks=1, acceptable_roi=15)
+    slice_blacklist = SliceBlacklist(campaign_id=long(campaign), min_clicks=int(clicks), acceptable_roi=int(roi))
     final_sql = slice_blacklist.final_sql()
 
     columns = d.describe_query(final_sql)
 
-    print 'columns', columns
-
     items = d.read(sql=final_sql, columns=columns)
 
-    print 'items', items
-
-    zones_black = []
-    for o, i, l in items:
-        print 'o - {o}, i - {i}, l - {l}'.format(o = o, i = i, l = l)
-        zones_black.append(o['Site'])
-    print zones_black
+    print "\n".join(map(lambda (o, i, l): o['Site'], items))
 
 if __name__ == '__main__':
     execute()
