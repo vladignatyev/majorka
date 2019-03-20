af91cf617cfeaa1793c6d2842c0cda70992591b2
Vitalii Ruzhnikov <varuzhnikov@gmail.com>
2019-03-20 14:47:08 +0400

    Correct file handling

--- tools/block_zones_for_campaign.py
+++ tools/block_zones_for_campaign.py
@@ -2,13 +2,13 @@ import argparse
 import sys
 
 parser = argparse.ArgumentParser(description="Block zones given via stdin or file for given campaign")
-parser.add_argument('input_file', nargs='?', type=argparse.FileType('r'))
+parser.add_argument('input_filename', nargs='?')
 
 args = parser.parse_args()
 zones = []
-if args.input_file:
-    print 'reading file'
-    zones = args.input_file.read()    
+if args.input_filename:
+    with open(args.input_filename) as f:
+        zones = f.read()    
 elif not sys.stdin.isatty():
     print 'reading stdin...'
     zones = sys.stdin.read()
