d50dd1aa04dba741d5907dd37285dc5b5c12cf89
Vladimir Ignatev <ya.na.pochte@gmail.com>
2019-04-03 11:42:02 +0400

    partial postback support

--- tools/supertest.py
+++ tools/supertest.py
@@ -130,6 +130,90 @@ class TrafficSamplerTest(EnvironmentTestCase):
             self.assertIn('http://test-url', response.headers['location'])
 
 
+class PartialParametersInPostback(EnvironmentTestCase):
+    @hang
+    def test_partial(self):
+        # loading fixture data
+        import random
+        import json
+        from furl import furl
+        from testtools.fixtures.redis_fixture import fixture_data as rfix
+
+
+        all_hits = [dict(flat_items(json.loads(rfix[k]))) for k in rfix.keys() if k.startswith('Hits') and '_counter' not in k]
+        all_conversions = [dict(flat_items(json.loads(rfix[k]))) for k in rfix.keys() if k.startswith('Conversion') and '_counter' not in k]
+
+
+        # build 'join table' by external_id
+        external_id_to_hit = { item['dimensions.external_id']: item for item in all_hits }
+        external_id_to_conversion = { item['external_id']: item for item in all_conversions }
+
+        print len(list(all_hits))
+        print len(list(all_conversions))
+
+        # create offer models from hit fixture
+        offer_ids = list(sorted(set([hit['destination_id'] for hit in all_hits])))
+        offer_models = {offer_id: MultiDimensionDistribution()  for offer_id in offer_ids}
+
+        print offer_ids
+
+        # train offer models on saved traffic
+        dimensions = [u'dimensions.zone', u'dimensions.connection_type', u'dimensions.langcode']
+        for hit in all_hits:
+            external_id = hit['dimensions.external_id']
+            conv = external_id in external_id_to_conversion.keys()
+            vec = [hit.get(d, None) for d in dimensions] + ['conv' if conv else 'noconv']
+            offer_models[hit['destination_id']].eat(vec)
+
+        # offer_models[offer_ids[1]].print_graph()
+
+        self.fixture.create_campaign(name='test campaign', alias='alias', offer_ids=[self.fixture.create_offer(name='test offer {i}'.format(i=i), url='http://test-url-{i}.com/?external_id={external_id}'.format(i=i, external_id='{external_id}')) for i, offer_id in enumerate(offer_ids)])
+
+        # create url template for campaign, that accepts connection type as a parameter
+        base_url = self.majorka.campaign_url(campaign='alias').add({
+            'connection_type': '{connection_type}'
+        })
+
+        filtered_samples = filter_dimension(samples_from_fixture(hit_samples), 'zone', ('', 'AB_TEST',))
+
+        sampler = BasicTrafficSampler(base_url=base_url, samples=with_cost(filtered_samples, value='0.005'))
+
+        logged_conversions = list(self.bus.multiread('Conversions', start=0))
+        self.assertEqual(len(logged_conversions), 0)
+
+        conv = 0
+        for i in range(1000):
+            request, sample = sampler.next()
+            response = sampler.send(request)
+            self.assertEqual(response.status_code, 302)
+
+            loc = furl(response.headers['Location'])
+            # print loc
+            offer_num = int(str(loc).split('http://test-url-', 1)[1].split('.com', 1)[0])
+            external_id = loc.args['external_id']
+
+            self.assertIn('http://test-url', str(loc))
+            vec = [sample.get(d, None) for d in dimensions] + ['conv']
+            # print vec
+            prob = offer_models[offer_ids[offer_num]].probability(vec)
+            # print prob
+
+            if random.random() <= prob:
+                # partial postback parameters as yobany propeller does it
+                postback_url = self.majorka.majorka_url().set(None, 'postback').add({
+                    'external_id': external_id,
+                    'revenue': '0.06',
+                    'status': 'lead'
+                })
+                print postback_url
+                response = requests.get(postback_url)
+                self.assertEqual(response.status_code, 200)
+                conv += 1
+
+        logged_conversions = list(self.bus.multiread('Conversions', start=0))
+        self.assertEqual(len(logged_conversions), conv)
+
+
 class ZoneSampleModelTest(EnvironmentTestCase):
     @hang
     def test_simple(self):
